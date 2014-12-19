e96f29697292ac4ffb8be75eeecc725a6c97faa1
Praveen Reddy <prreddy@amazon.com>
2014-12-19 11:55:22 +0530

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# 0.1.0
+
+Initial release of two-DBAE6530
+
+* Enhancements
+  * an enhancement
+
+* Bug Fixes
+  * a bug fix
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'two-DBAE6530'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures two-DBAE6530'
+long_description 'Installs/Configures two-DBAE6530'
+version          '0.1.0'
+
